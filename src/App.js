156da88ecd38e65783bc8ac4bea6f806949c1331
Rob Relyea <rob@relyeas.net>
2022-07-15 08:44:01 -0700

    simplify redirect code

--- src/App.js
+++ src/App.js
@@ -1,35 +1,21 @@
 import React from 'react';
-import BreadCrumbBar from './BreadCrumbBar.js';
-import People from './People.js';
 import './App.css';
 
+var redirectUrl = "https://covidsafe.fyi" + window.location.pathname;
 
+function redirect() {
+  window.location.replace(redirectUrl);
+}
 
 function App() {
+  window.setTimeout(redirect, 5000);
 
-  const supportAd = <>
-  <div className='tdAd' colSpan='2'><b>SUPPORT</b>: Please enable <a href='https://www.geekwire.com/2022/after-wife-got-cancer-microsoft-engineer-built-a-tool-to-locate-anti-covid-drug-for-immunocompromised/'>me</a> to provide this helpful info to you (ad free) by giving a small donation:</div>
-  <div className='tdAd'><b>Sponsor via</b> <a href='https://buymeacoffee.com/rrelyea'>buymeacoffee.com</a>, <a href='https://paypal.me/RobRelyea'>paypal</a>, or <a href='https://venmo.com/code?user_id=2295481921175552954'>venmo</a></div>
-  </>;
-
-  return (
-    <div className="App">
-      <BreadCrumbBar />
-      <h3 className="p-3 text-center">Track and Plan Vaccination Timing for Your Loved Ones</h3>
-      <div className="p-3 text-center">NOTE: don't use this as a definitive source yet...This site is a work in progress! Send any bug reports or questions to: rob@relyeas.net</div>
-      <div className="p-3 text-center">PRIVACY: Your data is never sent to our server. We don't store anything.</div>
-      <br/>
-      <People />
- 
-      <br />
-
-      {supportAd}
-      <br />
-
-      <div><b>Links:</b> <a href='https://www.cdc.gov/vaccines/covid-19/downloads/COVID-19-vacc-schedule-at-a-glance-508.pdf'>CDC Vaccination Schedule Diagram</a></div>
-      <br />
-    </div>
-  );
+  return <>
+      <div >
+        <h3>Redirecting in 5 seconds to new web address for site: <a href={redirectUrl}>{redirectUrl}</a></h3>
+        <div>Please update your existing links and bookmarks.</div>
+      </div>
+    </>;
 }
 
 export default App;
